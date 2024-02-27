d2027cebbcd46ccffd52777a466c20534152bc3b
Renat Prokopiev <prokop4891@gmail.com>
2024-02-27 15:52:57 +0300

    5th lect

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'myapp4',
+    'myapp5',
 ]
 
 MIDDLEWARE = [
@@ -104,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
